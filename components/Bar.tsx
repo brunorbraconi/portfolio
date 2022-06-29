7820c03011715c0c53b5262f6128372583cd221b
Bruno_Braconi <brunorbraconi@hotmail.com>
2022-06-29 05:35:30 -0300

    color bar changed

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -22,7 +22,7 @@ const Bar: FunctionComponent<{ value: ISkill }> = ({
   return (
     <div className="my-2 text-white bg-gray-300 rounded-full dark:bg-dark-300 dark:bg-black-500">
       <motion.div
-        className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-purple to-blue-700"
+        className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-purple to-cyan-500"
         style={{
           width: level
         }}
