9a49694786746c84a6195f8a2c5192ab44068bc4
Bruno_Braconi <brunorbraconi@hotmail.com>
2022-07-13 20:21:27 -0300

    fixed mail

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -52,7 +52,7 @@ const changeTheme = () => {
         </div>
         {/* Email button */}
         <button className='w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-purple to-cyan-500 focus:outline-none'
-        onClick={()=>window.open('mailto:  brunorbraconi@hotmail.com')}>E-mail me</button>
+        onClick={()=>window.open('mailto:brunorbraconi@hotmail.com')}>E-mail me</button>
         <button 
         onClick={changeTheme}
         className='w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-purple to-cyan-500'>Toggle Dark Theme</button>
